26f6187f61636de44b1470c1cd8d9ef7cbcc52a9
Thales Aguiar <thalesaguiar21@gmail.com>
2018-05-03 11:48:57 -0300

    Hibernate configuration of ACcount
    - Added anotations to Account class;
    - Added default constructor;
    - Removed old customized constructor;
    - Account class must match POJO style now;

--- Commons/src/domain/Account.java
+++ Commons/src/domain/Account.java
@@ -1,5 +1,8 @@
 package domain;
 
+import java.util.Random;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,26 +12,30 @@ public class Account {
 
 	public static final Double MAX_BALANCE = 10000000.0;
 	
-	@Id
-	@GeneratedValue
+	@Id @GeneratedValue
+	@Column(name = "id_account")
 	private Long id;
+	
+	@Column
 	private Double balance;
+	
+	@Column(nullable=false)
 	private Long accNumber;
+	
+	@Column(nullable=false)
 	private String ownerCpf;
+	
+	@Column
 	private String ownerName;
 	
-	public Account() {
-		id = Long.valueOf(0);
-		balance = 0.0;
-		accNumber = Long.valueOf(0);
-		ownerCpf = "";
-		ownerName = "";
-	}
+	public Account() {}
 	
-	public Account(Long id, Double balance, String ownerCpf, String ownerName) {
-		this.id = id;
+	public Account(double balance, String cpf, String ownerName) {
+		Random accNumGenerator = new Random(); 
+		this.id = Long.valueOf(0);
 		this.balance = balance;
-		this.ownerCpf = ownerCpf;
+		accNumber = accNumGenerator.nextLong();
+		ownerCpf = cpf;
 		this.ownerName = ownerName;
 	}
 	
